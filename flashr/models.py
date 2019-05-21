c3ba3f099cc27400094a7413bfbbb30675cb0394
Jhrohman <josephhrohman@gmail.com>
2019-05-21 16:42:56 -0700

    Fixing changes on merge

--- flashr/models.py
+++ flashr/models.py
@@ -1,45 +1,45 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
-# class Deck(models.Model):
-#   profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='deck')
-#   question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='deck')
-#   order_idx = models.IntegerField()
+# Create your models here.
+class Deck(models.Model):
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='deck')
+  question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='deck')
+  order_idx = models.IntegerField()
 
-#   def __str__(self):
-#     return self.order_idx
+  def __str__(self):
+    return self.order_idx
   
-# class Question(models.Model):
-#   author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='question')
-#   title = models.CharField()
-#   content = models.TextField()
-#   tags = models.ManyToManyField(Tag)
-#   #tags and questions must exist before being combined
-#     #question = //whatever from the form data
-#     #question.save()
-#     #tag = Tag.objects.create(content='HTML', color_code=//color function thing)
-#     #question.tags.add(tag)
-#   def __str__(self):
-#     return self.title
+class Question(models.Model):
+  author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='question')
+  title = models.CharField()
+  content = models.TextField()
+  tags = models.ManyToManyField(Tag)
+  #tags and questions must exist before being combined
+    #question = //whatever from the form data
+    #question.save()
+    #tag = Tag.objects.create(content='HTML', color_code=//color function thing)
+    #question.tags.add(tag)
+  def __str__(self):
+    return self.title
     
-# class Pain(models.Model):
-#   profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='pain')
-#   question = models.ForeignKey(Question, on_delete=models. CASCADE, related_name='pain')
-#   pain_level = models.IntegerField()
-#   time_stamp = models.DateField.auto_now_add()
+class Pain(models.Model):
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='pain')
+  question = models.ForeignKey(Question, on_delete=models. CASCADE, related_name='pain')
+  pain_level = models.IntegerField()
+  time_stamp = models.DateField.auto_now_add()
 
-#   def __str__(self):
-#     return self.pain_level
+  def __str__(self):
+    return self.pain_level
 
-# class Answer(models.Model):
-#   author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='answer')
-#   question = models.ForeignKey(Question, on_delete=models. CASCADE, related_name='answer')
-#   public = models.BooleanField()
-#   content = models.TextField()
+class Answer(models.Model):
+  author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='answer')
+  question = models.ForeignKey(Question, on_delete=models. CASCADE, related_name='answer')
+  public = models.BooleanField()
+  content = models.TextField()
 
-#   def __str__(self):
-#     return self.content
+  def __str__(self):
+    return self.content
 
-# class Tag(models.Model):
-#   content = models.TextField()
-#   color_code = models.CharField(max_length=100)
+class Tag(models.Model):
+  content = models.TextField()
+  color_code = models.CharField(max_length=100)
