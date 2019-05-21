de5acfdb9e2764dfbfe3db73e9be6cfead8fb4e0
josephhrohman <josephhrohman@gmail.com>
2019-05-21 16:33:04 -0700

    Revert "J sprint 1"

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
   path('register/', views.register, name='register'),
   path('login/', views.login, name='login'),
   path('logout/', views.logout, name='logout'),
-  path('profile/', views.profile, name='profile')
 ]
