364f951280034c27511426dce6558067080800ff
zack <zack.etc@gmail.com>
2019-05-22 11:15:48 -0700

    question model push for testing

--- flashr/views.py
+++ flashr/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 #deck_create needs to recieve a tag
 #deck_create pain level pain_level issue discuss
+#deck functions w i p, commented out
+
 
 # Create your views here.
 #Questions
@@ -9,18 +11,18 @@ def question_show(request, pk):
   return render(request, 'flashr/card.html', {'question': question})
 
 #Deck
-def deck_show(request, pk):
-  deck = Deck.objects.filter(profile = user.profile)
-  return render(request, 'flashr/card_deck.html', {'deck': deck})
+# def deck_show(request, pk):
+#   deck = Deck.objects.filter(profile = user.profile)
+#   return render(request, 'flashr/card_deck.html', {'deck': deck})
 
-def deck_create(request, tag): #is this correct?
-  Deck.objects.filter(profile = user.profile).delete()
+# def deck_create(request, tag): #is this correct?
+#   Deck.objects.filter(profile = user.profile).delete()
 
-  deck = Question.objects.filter(tags__content=tag)
-    .filter(//NOT SURE DUDE)
-    .extends(Question.objects.filter(tags__content=tag)
-    .order_by(pain.level))
+#   deck = Question.objects.filter(tags__content=tag)
+#     .filter(//NOT SURE DUDE)
+#     .extends(Question.objects.filter(tags__content=tag)
+#     .order_by(pain.level))
 
-  for idx, card in enumerate(deck):
-    Deck.objects.create(profile=user.profile, question=deck[idx], order_idx=(idx+1))
-  return redirect('deck_show')
+#   for idx, card in enumerate(deck):
+#     Deck.objects.create(profile=user.profile, question=deck[idx], order_idx=(idx+1))
+#   return redirect('deck_show')
