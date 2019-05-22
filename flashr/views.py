67aeffed8142bb1ab131cdff1a4a4682445a5693
zack <zack.etc@gmail.com>
2019-05-21 17:20:13 -0700

    begins view functions

--- flashr/views.py
+++ flashr/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+#deck_create needs to recieve a tag
+#deck_create pain level pain_level issue discuss
 
 # Create your views here.
+#Questions
+def question_show(request, pk):
+  question = Question.objects.get(pk=pk)
+  return render(request, 'flashr/card.html', {'question': question})
+
+#Deck
+def deck_show(request):
+  deck = Deck.objects.filter(profile = user.profile)
+  return render(request, 'flashr/card_deck.html', {'deck': deck})
+
+def deck_create(request, tag): #is this correct?
+  old_deck = Deck.objects.filter(profile = user.profile).delete()
+  deck = Question.objects.filter(tags__content=tag).order_by(pain.level)
+  for idx, card in enumerate(deck):
+    Deck.objects.create(profile=user.profile, question=deck[idx], order_idx=(idx+1))
+  return redirect('deck_show')
