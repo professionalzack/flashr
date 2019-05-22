d4db038a57bbe64a180f4b4cbe2f85137f475729
Jerusalem Lipardo <beigecolor29@gmail.com>
2019-05-22 08:43:11 -0700

    accounts profile is connected

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('register/', views.register, name='register'),
   path('login/', views.login, name='login'),
   path('logout/', views.logout, name='logout'),
+  path('profile/', views.profile, name='profile')
 ]
